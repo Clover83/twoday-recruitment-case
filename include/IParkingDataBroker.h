941e65a63f65f96ce67b1d190851ee28628cfc7f
Emily Sundberg <emilysundberg83@gmail.com>
2024-05-06 10:42:47 +0200

    IParkingDataBroker briefs

--- include/IParkingDataBroker.h
+++ include/IParkingDataBroker.h
@@ -4,11 +4,22 @@
 
 struct HouseInfo;
 
+/// @brief Interface between ParkingHouse and your storage system.
 class IParkingDataBroker
 {
 public:
     virtual ~IParkingDataBroker() {}
+
+    /// @brief Search for spot using partial data.
+    /// @param dataToSearchFor Partial data is OK.
+    /// @return BrokerResult and the spot data which was found, if any.
     virtual std::pair<BrokerResult, ParkingSpotData> findSpot(const ParkingSpotData& dataToSearchFor) const = 0;
+
+    /// @brief Is called when an entry has been validated and can be stored.
+    /// @param entryData 
     virtual void onValidEntry(const ParkingSpotData& entryData) = 0;
+
+    /// @brief Is called when an exit has been validated and can be stored.
+    /// @param exitData 
     virtual void onValidExit(const ParkingSpotData& exitData) = 0;
 };
